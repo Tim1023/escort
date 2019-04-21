d3f60eeaf81af93158bbd91a180732ea77e08367
Tim1023 <timzdeveloper23@gmail.com>
2019-04-22 00:05:46 +1200

    Revert "Navigation: Remove search from navigation."
    This reverts commit 00bb2ffdbfc7e1b21227d5b905b19939026e182a.

--- src/common/navigation/index.js
+++ src/common/navigation/index.js
@@ -5,8 +5,11 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/styles';
 import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import {fade} from '@material-ui/core/styles/colorManipulator';
+import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import Link from '@material-ui/core/Link';
@@ -25,16 +28,52 @@ const useStyles = makeStyles(theme => ({
     marginLeft: -12,
     marginRight: 20,
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+  },
+  searchIcon: {
+    width: theme.spacing(9),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(10),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
   sectionDesktop: {
     display: 'none',
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up('md')]: {
       display: 'flex',
       alignItems: 'center',
     },
   },
   sectionMobile: {
     display: 'flex',
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up('md')]: {
       display: 'none',
     },
   },
@@ -112,7 +151,9 @@ function ButtonAppBar({isAuthenticated}) {
       <MenuItem component={Link} href="/pricing">
         Pricing
       </MenuItem>
-
+      <MenuItem onClick={handleLanguageMenuOpen}>
+        Language
+      </MenuItem>
       {isAuthenticated
         ? <MenuItem onClick={handleProfileMenuOpen}>
           <IconButton color="inherit">
@@ -133,9 +174,7 @@ function ButtonAppBar({isAuthenticated}) {
           </MenuItem>
         </React.Fragment>
       }
-      <MenuItem onClick={handleLanguageMenuOpen}>
-        Language
-      </MenuItem>
+
     </Menu>
   );
 
@@ -146,7 +185,18 @@ function ButtonAppBar({isAuthenticated}) {
           <Typography className={classes.title} variant="h6" color="inherit" noWrap>
             Escort
           </Typography>
-
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon/>
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
           <div className={classes.sectionDesktop}>
             <Link
               href='/pricing'
@@ -155,6 +205,14 @@ function ButtonAppBar({isAuthenticated}) {
             >
               Pricing
             </Link>
+            <IconButton
+              aria-owns={isProfileMenuOpen ? 'material-appbar' : undefined}
+              aria-haspopup="true"
+              onClick={handleLanguageMenuOpen}
+              color="inherit"
+            >
+              <LanguageIcon/>
+            </IconButton>
             {isAuthenticated
               ? <IconButton
                 aria-owns={isProfileMenuOpen ? 'material-appbar' : undefined}
@@ -181,16 +239,7 @@ function ButtonAppBar({isAuthenticated}) {
                   Login
                 </Link>
               </React.Fragment>
-
             }
-            <IconButton
-              aria-owns={isProfileMenuOpen ? 'material-appbar' : undefined}
-              aria-haspopup="true"
-              onClick={handleLanguageMenuOpen}
-              color="inherit"
-            >
-              <LanguageIcon/>
-            </IconButton>
           </div>
           <div className={classes.sectionMobile}>
             <IconButton aria-haspopup="true" onClick={handleMobileMenuOpen} color="inherit">
